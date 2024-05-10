aee51f45e3b80d35fd132fba2603513b84f1ccb7
Wins <163721633+Wins010@users.noreply.github.com>
2024-05-10 16:56:53 +0400

    Add files via upload

---
+++ hypotenuse.py
@@ -0,0 +1,5 @@
+altside = int(input("Enter the length of the alternate side: "))
+baseside = int(input("Enter the length of the base side: "))
+c=((altside**2)+(baseside**2))
+hyp=c**(1/2)
+print("Hypotenuse of the Triangle: ", hyp)
